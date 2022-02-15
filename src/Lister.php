5a5ba5651d3a9ddc36eb282babbb6fa3e73acb5e
Brian Faust <hello@basecode.sh>
2022-02-15 05:39:04 +0200

    add `branch` argument

--- src/Lister.php
+++ src/Lister.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Collection;
 
 interface Lister
 {
-    public function byUser(string $uuid, ?array $options): Collection;
+    public function byUser(string $uuid, string $branch, ?array $options): Collection;
 
-    public function byTeam(string $uuid, ?array $options): Collection;
+    public function byTeam(string $uuid, string $branch, ?array $options): Collection;
 }
